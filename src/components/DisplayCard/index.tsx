8abcda4d38171f7fb8546df6923cdf4419f17eed
Manish Aneja <manishekaneja@gmail.com>
2021-06-15 11:23:37 +0530

    💄 Add elevation to display card

--- src/components/DisplayCard/index.tsx
+++ src/components/DisplayCard/index.tsx
@@ -1,12 +1,23 @@
 import React, {FC} from 'react';
 import {StyleProp, View, ViewStyle} from 'react-native';
-import {bg, border, flx, m, px, py} from '../../styles';
+import {bg, border, flx, m, mx, my, px, py} from '../../styles';
 
 const DisplayCard: FC<{
   style?: StyleProp<ViewStyle>;
 }> = ({children, style}) => {
   return (
-    <View style={[bg.white, border.r10, flx.f1, py.d14, px.d14, m.d2, style]}>
+    <View
+      style={[
+        bg.white,
+        border.r10,
+        flx.f1,
+        py.d14,
+        px.d14,
+        my.d5,
+        mx.d2,
+        {elevation: 2},
+        style,
+      ]}>
       {children}
     </View>
   );
